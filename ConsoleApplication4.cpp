2f43d9aee8efe42ab2d96703b48f09563d0b1194
jensen0jensen <90957898+jensen0jensen@users.noreply.github.com>
2022-01-19 20:51:23 -0500

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,16 @@
+/*
+/   James Jensen
+/
+/   My Hello World program using C++
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World! Please enter a number!\n";
+    double input;
+    cin >> input;
+    cout << "You entered in " << input;
+    return 0;
+}
