a0ba6ee4105359b3c5905d572bab706811610101
jensen0jensen <90957898+jensen0jensen@users.noreply.github.com>
2022-01-25 14:39:57 -0500

    Add files via upload

---
+++ AvgOfValues.cpp
@@ -0,0 +1,37 @@
+/*
+Student Name:  James Jensen
+Date: 25/01/2022
+Lab Assignment: Lab 01 - Ch2 Intro
+Project Name: Lab01_03Proj
+Description: This calculates the sum of five hardcoded values
+Credits: Not Applicable
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double values[] = { 28,32,37,24,33 };
+	double total = 0;
+
+	for (int i = 0; i < (sizeof(values) / sizeof(double)); i++)
+	{
+		total += values[i];
+	}
+
+	cout << "This program calculates the average of five numbers.\n" << endl;
+	cout << "The sum of the five numbers is: " << total << endl;
+	cout << "The average of the five numbers is: " << total / (sizeof(values)/sizeof(double)) << endl;
+
+	system("pause");
+
+	return 0;
+}
+/*
+Proof:
+This program calculates the average of five numbers.
+
+The sum of the five numbers is: 154
+The average of the five numbers is: 30.8
+Press any key to continue . . .
+*/
